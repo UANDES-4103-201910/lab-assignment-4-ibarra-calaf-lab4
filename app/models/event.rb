74c27d70f2661e14c3bd503ffbf1ccab139f4a4e
JavierIbarra <jaibarra1@miuandes.cl>
2019-04-03 10:34:10 -0300

    event venue validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,5 @@ class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
 
-  validates: name, capacity, presence: true
-	validates: name, length: {minimum: 20} 
-	validates: capacity, numericality: {only_integer: true}, numericality: {greater_than: 0} 
+ 
 end
--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,2 +1,5 @@
 class EventVenue < ApplicationRecord
+    validates: name, capacity, presence: true
+	validates: name, length: {minimum: 20} 
+	validates: capacity, numericality: {only_integer: true}, numericality: {greater_than: 0} 
 end
