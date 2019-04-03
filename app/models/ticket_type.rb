bac49f1dcd0b54ed1ce8d7266cf3f378f8d80515
JavierIbarra <jaibarra1@miuandes.cl>
2019-04-03 10:38:10 -0300

    tiket type validation

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,4 +1,7 @@
 class TicketType < ApplicationRecord
   belongs_to :event
   belongs_to :ticket_zone
+
+  validates: price, presence: true
+	validates: price, numericality: {only_integer: true}, numericality: {greater_than: 0}
 end
