06aa1dc976b6bdba557fd388af7df0eea77b07ff
JavierIbarra <jaibarra1@miuandes.cl>
2019-04-03 10:27:44 -0300

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ApplicationRecord
   has_many :orders
+  validates :email, presence: true, email: true
+  validates :password, presence: true, format:{with: /^[A-Za-z0-9]+$/}, length: {in:8..12}
+  validates : phone, presence: true, numericality: {only_integer: true}, length: {in:9..12}
 end
