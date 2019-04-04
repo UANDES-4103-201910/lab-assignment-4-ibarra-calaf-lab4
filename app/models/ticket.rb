4c46e84370b14e17ec8d11d8d9b001be9da2ba4a
JavierIbarra <jaibarra1@miuandes.cl>
2019-04-03 22:54:32 -0300

    updating ticket validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,16 +2,18 @@ class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
   before_save :created_after
-  before_save :bought later
+  before_save :bought_later
   
   def created_after
       if Order.find(order_id).created_at>Event.find(TicketType.find(ticket_type_id).event_id).start_date
         puts("A ticket cannot be created after the start date of the associated event")
         throw :abort
+      end
   end
   def bought_later
       if Event.find(TicketType.find(ticket_type_id).event_id).start_date>=Date.today
         puts("No ticket can be bought after the start date of the event")
         throw :abort
+      end
   end
 end
