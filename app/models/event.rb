1a5e967e39486661392ee09062f6376a77aad885
JavierIbarra <jaibarra1@miuandes.cl>
2019-04-03 22:12:05 -0300

    updating event validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,19 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+  validate :later_than_current_date
+  validate :two_or_more_events
+  validates :start_date, presence: true
+
+  def later_than_current_date
+    if start_date<Date.today
+      erros.add(:start_date,"Date can't be before current date")
+    end
+  end
+
+  def two_or_more_events
+    if Event.where(event_venue_id:event_venue_id, start_date:start_date).count()>0
+      erros.add("There cannot be two or more events hosted in the same venue with the same start date")
+    end
+  end
 end
